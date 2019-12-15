d12b59bb47fdb145e1b30ab2e36c2e0dee6723b3
Serina Grill <gril0044@umn.edu>
2019-12-14 22:23:19 -0600

    deploying - adding procfile

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,15 @@
 '''Main application for twitoff'''
 import uuid
 from decouple import config
+from dotenv import load_dotenv
 # import flask package, flask makes app objects
 from flask import Flask, render_template, request
 #import from same directory
 from .models import DB, User, Tweet
+from .predict import predict_user
 from .twitter import add_or_update_user
+
+load_dotenv()
 
 def create_app():
     # create Flask web server, makes application
@@ -16,21 +20,18 @@ def create_app():
     # app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
-    #routes determine location
-    # @app.route('/')
-    # def index():
-    #     # rand_name = str(uuid.uuid4())
-    #     # rand_u = User(name=rand_name)
-    #     # DB.session.add(rand_u)
-    #     # DB.session.commit()
-    #     return 'Index Page'
-
     @app.route('/')
     def root():
-        users = User.query.all()
-        return render_template('base.html', title='Home', users=users)
+        DB.create_all()
+        return render_template('base.html', title='Home', users=User.query.all())
 
-    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='hello')
+
+    @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
         # if user retrieval fails
@@ -38,21 +39,28 @@ def create_app():
         try:
             if request.method == 'POST':
                 add_or_update_user(name)
-                message = "User {} successfully added!".format(name)
+                message = f"User {name} successfully added!"
             tweets = User.query.filter(User.name == name).one().tweets
         except Exception as e:
-            message = "Error adding {}: {}".format(name,e)
+            message = f"Error adding {name}: {e}"
             tweets=[]
-        return render_template('user.html', title=name, tweets=tweeets,
+        return render_template('user.html', title=name,
+                                tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = "Cannot compare same user"
+        else:
+            prediction = predict_user(user1, user2, tweet_text)
+            message = '"{}" is more likely to be said by {} than {}'.format(
+            request.values['tweet_text'], user1 if prediction else user2,
+            user2 if prediction else user1)
+        return render_template('prediction.html', title="Results",
         message=message)
 
-    @app.route('/reset')
-    def reset():
-        DB.drop_all()
-        DB.create_all()
-        return render_template('base.html', title='DB Reset', users=[])
-
-    @app.route('/about')
-    def preds():
-        return render_template('home.html')
     return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -13,26 +13,53 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
-def add_or_update_user(username):
-    '''Add or update a user and their Tweets, or else error'''
+# def add_or_update_user(username):
+#     '''Add or update a user and their Tweets, or else error'''
+#     try:
+#         twitter_user=TWITTER.get_user(username)
+#         db_user=(User.query.get(twitter_user.id) or
+#         User(id=twitter_user.id, name=username))
+#         DB.session.add(db_user)
+#         tweets=twitter_user.timeline(count=200,
+#                                         exclude_replies=True,
+#                                         include_rts=False,
+#                                         tweet_mode='extended',
+#                                         since_id=db_user.newest_tweet_id)
+#         if tweets:
+#             db_user.newest_tweet_id = tweets[0].id
+#         for tweet in tweets:
+#             #Calculate embedding on the full tweet
+#             embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+#             db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+#             embedding=embedding)
+#             db_user.tweets.append(db_tweet)
+#             DB.session.add(db_tweet)
+#     except Exception as e:
+#         print(f'Error processing {username} : {e}'.format())
+#         raise e
+#     else:
+#         DB.session.commit()
+
+def add_or_update_user(name):
+    """Add or update a user and their Tweets, or else error"""
     try:
-        twitter_user=TWITTER.get_user(username)
-        db_user=(User.query.get(twitter.user.id) or
-        User(id=twitter_user.id, name=username))
+        twitter_user=TWITTER.get_user(name)
+        db_user=(User.query.get(twitter_user.id) or
+        User(id=twitter_user.id, name=name))
         DB.session.add(db_user)
-        tweets=twitter_user.timeline(count=200, exclude_replies=True ,
-        include_rts=False, tweet_mode='extended', since_id=db_user)
+        tweets = twitter_user.timeline(count=200, exclude_replies=True,
+        include_rts=False, since_id=db_user.newest_tweet_id)
         if tweets:
             db_user.newest_tweet_id = tweets[0].id
         for tweet in tweets:
             #Calculate embedding on the full tweet
-            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+            embedding = BASILICA.embed_sentence(tweet.text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.text[:300],
             embedding=embedding)
             db_user.tweets.append(db_tweet)
             DB.session.add(db_tweet)
     except Exception as e:
-        print('Error processing {} : {}'.format(username,e))
+        print('Error processing {name}: {e}')
         raise e
     else:
         DB.session.commit()
